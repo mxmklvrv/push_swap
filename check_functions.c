1898293a4eac734df81dee43b7f070c1620cf2c4
Maxim Kleverov <maximkleverov@icloud.com>
2025-06-10 18:15:01 +0300

    move added

--- check_functions.c
+++ check_functions.c
@@ -6,7 +6,7 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 15:44:42 by mklevero          #+#    #+#             */
-/*   Updated: 2025/06/09 18:59:27 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/06/10 18:14:03 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,7 @@ void	check_sort(t_node *a_stack, t_node *b_stack)
 {
 	if (!is_sorted(a_stack))
 	{
+		
 	}
 }
 
@@ -80,4 +81,131 @@ bool	is_sorted(t_node *stack)
 		stack = stack->next;
 	}
 	return (true);
+}
+
+// main swap function 
+static void	swap(t_node **stack)
+{
+	t_node	*first;
+	t_node	*second;
+	
+	if (*stack == NULL || (*stack)->next == NULL)
+		return ; 
+	first = *stack;
+	second = first->next;
+	first->next = second->next;
+	second->previous = first->previous;
+	if (second->next != NULL)
+		second->next->previous = first;
+	first->previous = second;
+	second->next = first;
+	*stack = second;
+}
+
+void	swap_a_stack(t_node **a_stack)
+{
+	swap(a_stack);
+	write(1, "sa\n", 3);
+}
+void	swap_b_stack(t_node **b_stack)
+{
+	swap(b_stack);
+	write(1, "sb\n", 3);
+}
+void	swap_both(t_node **a_stack, t_node **b_stack)
+{
+	swap(a_stack);
+	swap(b_stack);
+	write(1, "ss\n", 3);
+}
+// rotate function
+static void rotate(t_node **stack)
+{
+	t_node *first;
+	t_node *last;
+	
+	if (*stack == NULL || (*stack)->next == NULL)
+		return ; 
+	first = *stack;
+	last = ft_stack_last(*stack);
+	*stack = first->next;
+	(*stack)->previous = NULL;
+	last->next = first;
+	first->previous = last;
+	first->next = NULL;
+}
+void rotate_a_stack(t_node **a_stack)
+{
+	rotate(a_stack);
+	write(1, "ra\n", 3);
+}
+void rotate_b_stack(t_node **b_stack)
+{
+	rotate(b_stack);
+	write(1, "rb\n", 3);
+}
+void rotate_both(t_node **a_stack, t_node **b_stack)
+{
+	rotate(a_stack);
+	rotate(b_stack);
+	write(1, "rr\n", 3);
+}
+// rev_rot function
+static void rev_rotate(t_node **stack)
+{
+	t_node *last;
+	
+	if (*stack == NULL || (*stack)->next == NULL)
+		return ; 
+	last = ft_stack_last(*stack);
+	last->previous->next = NULL;
+	last->next = *stack;
+	last->previous = NULL;
+	*stack = last;
+	last->next->previous = last;	
+}
+void	rev_rotate_a_stack(t_node **a_stack)
+{
+	rev_rotate(a_stack);
+	write(1, "rra\n", 4);
+}
+void	rev_rotate_b_stack(t_node **b_stack)
+{
+	rev_rotate(b_stack);
+	write(1, "rrb\n", 4);
+}
+void	rev_rotate_both(t_node **a_stack, t_node **b_stack)
+{
+	rev_rotate(a_stack);
+	rev_rotate(b_stack);
+	write(1, "rrr\n", 4);
+}
+// push function
+static void push(t_node **dst, t_node **src)
+{
+	t_node *to_move;
+	
+	if(*src == NULL)
+		return ;
+	to_move = *src;
+	*src = to_move->next;
+	if (*src != NULL)
+		(*src)->previous = NULL;
+	to_move->next = NULL;
+	if (*dst != NULL)
+		(*dst)->previous = to_move;
+	to_move->next = *dst;
+	to_move->previous = NULL;
+	*dst = to_move;
+}
+void push_a_stack(t_node **a_stack, t_node **b_stack)
+{
+	push(a_stack, b_stack);
+	write(1, "pa\n", 3);
+}
+
+void push_b_stack(t_node **a_stack, t_node **b_stack)
+{
+	push(b_stack, a_stack);
+	write(1, "pb\n", 3);
 }
