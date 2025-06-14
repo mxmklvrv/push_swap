c6945bcbe3773f85a2550c65784ba0d7a869a07d
Maxim Kleverov <mklevero@student.hive.fi>
2025-06-14 13:23:05 +0300

    some cleaning

--- sort_op.c
+++ sort_op.c
@@ -6,7 +6,7 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 12:19:47 by mklevero          #+#    #+#             */
-/*   Updated: 2025/06/13 17:28:51 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/06/14 13:21:28 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,31 +42,7 @@ t_node	*find_small_num(t_node *stack)
 	}
 	return (small);
 }
-void	turk_sort(t_node **a_stack, t_node **b_stack)
-{
-	int		len;
-	t_node	*small;
 
-	len = stack_len(*a_stack);
-	while (len-- > 3)
-		push_b_stack(b_stack, a_stack);
-	sort_three(a_stack);
-	while (*b_stack)
-	{
-		prep_nodes(*a_stack, *b_stack);
-		push_back(a_stack, b_stack);
-	}
-	set_pos(*a_stack);
-	small = find_small_num(*a_stack);
-	if (small->above_mid)
-	{
-		while (*a_stack != small)
-			rotate_a_stack(a_stack);
-	}
-	else
-		while (*a_stack != small)
-			rev_rotate_a_stack(a_stack);
-}
 
 void	prep_nodes(t_node *a_stack, t_node *b_stack)
 {
@@ -159,47 +135,6 @@ void	set_cheapest(t_node *b_stack)
 		b_stack = b_stack->next;
 	}
 	option->cheapest = true;
-}
-
-void	push_back(t_node **a_stack, t_node **b_stack)
-{
-	t_node	*cheapest;
-	t_node	*tg_in_a;
-
-	cheapest = find_cheapest(*b_stack);
-	tg_in_a = cheapest->target_node;
-	while (*a_stack != tg_in_a && *b_stack != cheapest)
-	{
-		if (tg_in_a->above_mid && cheapest->above_mid)
-			rotate_both(a_stack, b_stack);
-		else if (!tg_in_a->above_mid && !cheapest->above_mid)
-			rev_rotate_both(a_stack, b_stack);
-		else
-			break ;
-	}
-	set_pos(*a_stack);
-	set_pos(*b_stack);
-	final_touch(a_stack, b_stack, cheapest, tg_in_a);
-	push_a_stack(a_stack, b_stack);
-}
-
-void	final_touch(t_node **a_stack, t_node **b_stack, t_node *cheapest,
-		t_node *tg_in_a)
-{
-	while (*a_stack != tg_in_a)
-	{
-		if (tg_in_a->above_mid)
-			rotate_a_stack(a_stack);
-		else
-			rev_rotate_a_stack(a_stack);
-	}
-	while (*b_stack != cheapest)
-	{
-		if (cheapest->above_mid)
-			rotate_b_stack(b_stack);
-		else
-			rev_rotate_b_stack(b_stack);
-	}
 }
 
 t_node	*find_cheapest(t_node *stack)
